65e68e0b936c114ce83760aaa2b3ae1477e5668c
Jai Kumar <your_email@gmail.com>
2026-07-04 03:14:59 +0530

    Setup Flask project with MySQL integration

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template
+from config import Config
+from database.db import mysql
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+mysql.init_app(app)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/testdb")
+def testdb():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT VERSION()")
+        version = cur.fetchone()
+        cur.close()
+        return f"MySQL Connected Successfully! Version: {version[0]}"
+    except Exception as e:
+        return f"Database Connection Error: {e}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = "smartattendance123"
+
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "Kumarjai@4627"      # Apna actual password likhna
+    MYSQL_DB = "attendance_system"
